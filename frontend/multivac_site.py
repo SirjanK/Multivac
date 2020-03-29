3b8126edccc78bc28dfa1533c37f560710b8108e
SirjanK <sxkafle12@gmail.com>
2020-03-28 20:16:57 -0700

    Render template through flask

--- frontend/multivac_site.py
+++ frontend/multivac_site.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
+
+import frontend.frontend_constants as constants
 
 
 app = Flask(__name__)
@@ -6,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def render_index_page():
-    return 'Hello world'
+    return render_template(constants.INDEX_HTML)
 
 
 if __name__ == '__main__':
