e11bd71a3045de25b0a2047079242c8d4b5646eb
SirjanK <sxkafle12@gmail.com>
2020-04-05 14:54:33 -0700

    Get form choices working

--- frontend/multivac_site.py
+++ frontend/multivac_site.py
@@ -2,13 +2,27 @@ from flask import Flask, render_template
 
 import frontend.frontend_constants as constants
 
+from agents.agent_registry import AGENTS
+from environment.environment_registry import ENVIRONMENTS
+
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 
 @app.route('/')
 def render_index_page():
-    return render_template(constants.INDEX_HTML)
+    # Gather options for environment name
+    environment_name_options = sorted(ENVIRONMENTS.keys())
+
+    # Gather options for agent name
+    agent_name_options = sorted(AGENTS.keys())
+
+    return render_template(
+        constants.INDEX_HTML,
+        environment_name_options=environment_name_options,
+        agent_name_options=agent_name_options
+    )
 
 
 if __name__ == '__main__':
